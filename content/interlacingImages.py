c51093c1220d8bc6ecf78765890da06ffcb3ff2e
Rezvan Joshaghani <8338655+rezvanjoshaghani@users.noreply.github.com>
2021-12-18 20:41:28 -0700

    added interlace picture

--- content/interlacingImages.py
+++ content/interlacingImages.py
@@ -10,17 +10,25 @@ def main():
                   "pics/b-004.png",
                   "pics/b-005.png"
                   ]
-    images=[]
+    images = []
     for path in image_path:
         images.append(cv2.imread(path))
     h, w = images[0].shape[:2]
-    print(h, w)
-    imRes= interlace(images, h, w)
-    cv2.imwrite("test.png",imRes )
-    cv2.imshow("Window Name",imRes)
+    print(w, h)
+    imRes = interlace(images, h, w)
+    im=combine(images)
+    write_image("test.png", imRes)
+    cv2.imshow("Window Name", imRes)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
+
+def write_image(path, img):
+    # img = img*(2**16-1)
+    # img = img.astype(np.uint16)
+    # img = img.astype(np.uint8)
+    img = cv2.convertScaleAbs(img, alpha=(255.0))
+    cv2.imwrite(path, img)
 
 def interlace(images, h, w):
     inter = np.empty((h, w, 3), images[0].dtype)
@@ -35,14 +43,19 @@ def interlace(images, h, w):
     return b
 
 
-# def interlace(images, h, w):
-#     inter = np.empty((h, w, 3), images[0].dtype)
-#     for i in range(0,len(images)):
-#         inter[:h, i:w:5, :] = images[0][:h, i:w:5, :]
-#     return inter.astype(np.float32) / 255
-#
-#     b = np.float32(inter) / 255.0
-#     return b
+def interlace_4px_line_5px_gap(images, h, w):
+    inter = np.empty((h, w, 3), images[0].dtype)
+    inter[:h, 300:int(np.ceil(h/2+300)), :] = images[0][:h,300:int(np.ceil(h/2+300)), :]
+
+    # for i in range(0, len(inter[0])):
+    return inter.astype(np.float32) / 255
+
+def combine(images):
+    im = cv2.addWeighted(images[0], 0.5, images[1], 0.5, 0.0)
+    im = cv2.addWeighted(im, 0.5, images[2], 0.5, 0.0)
+    im = cv2.addWeighted(im, 0.5, images[3], 0.5, 0.0)
+    im = cv2.addWeighted(im, 0.5, images[4], 0.5, 0.0)
+    return im
 
 
 if __name__ == "__main__":
